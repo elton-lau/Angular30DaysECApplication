1d225074cba5575bdff06069081335d019385153
Elton Lau <twilightlau94@gmail.com>
2020-04-12 20:48:43 +0800

    finished project

---
+++ src/app/app-path.const.ts
@@ -0,0 +1,31 @@
+export const appPath = {
+    
+    home: '',
+    
+    products: 'products',
+
+    login: 'login',
+    // 購物車
+    cart: 'cart',
+
+    // 結帳
+    checkout: 'checkout',
+
+    // 結帳流程
+    checkoutFlow: {
+
+        // 運送
+        customerInfo: 'customer-info',
+
+        // 付款
+        paymentInfo: 'payment-info',
+
+        // 發票
+        receiptInfo: 'receipt-info'
+
+    },
+
+    // 結帳成功
+    success: 'success'
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,39 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { appPath } from './app-path.const'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: appPath.home,
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: appPath.products,
+    loadChildren: () => import('./product-section/product-section.module').then(m => m.ProductSectionModule)
+  },
+  {
+    path: appPath.login,
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  },
+  {
+    path: appPath.cart,
+    loadChildren: () => import('./cart/cart.module').then(m => m.CartModule)
+  },
+  {
+    path: appPath.checkout,
+    loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule)
+  },
+  {
+    path: appPath.success,
+    loadChildren:  () => import('./success/success.module').then(m => m.SuccessModule)
+  },
+  {
+    path: '**',
+    redirectTo: appPath.home,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
